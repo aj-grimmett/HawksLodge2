8fb586589101a1028bab8636106881be50812c93
agrim32 <56181107+agrim32@users.noreply.github.com>
2021-12-26 23:38:07 -0500

    added js file and hamburger menu that isn't functional

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navMenuclassList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link-wrapper").forEach (n => n.
-    addEventListener("click",() => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+
