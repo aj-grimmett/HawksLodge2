347822dcadc37f96f6f635be24e9cc80e26f2313
aj grimmett <ajgrimmett23@gmail.com>
2023-06-04 13:40:42 -0400

    hamburger menu functional

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
-
-
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
